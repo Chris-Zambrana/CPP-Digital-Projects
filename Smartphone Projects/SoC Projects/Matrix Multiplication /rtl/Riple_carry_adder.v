8cc3775d51bf5b620ad276d8082f004f86235ffc
Chris-Zambrana <crzambrana7@gmail.com>
2025-09-11 18:27:35 -0700

    Update Riple_carry_adder.v

--- Smartphone Projects/SoC Projects/Matrix Multiplication /rtl/Riple_carry_adder.v
+++ Smartphone Projects/SoC Projects/Matrix Multiplication /rtl/Riple_carry_adder.v
@@ -21,39 +21,40 @@
 
 
 module Riple_carry_adder(
-input[3:0] a, b,
+input [3:0] a, b,
 input c_in,
 output [3:0] s,
 output c_out
 );
-wire [3:0] c;
+wire [2:0] c;
 
 project_1 FA0 (
 .a(a[0]),
 .b(b[0]),
 .c_in(c_in),
 .s(s[0]),
-.c_out(c[1])
+.c_out(c[0])
 );
 
 project_1 FA1 (
 .a(a[1]),
 .b(b[1]),
-.c_in(c[1]),
+.c_in(c[0]),
 .s(s[1]),
-.c_out(c[2])
+.c_out(c[1])
 );
-   project_1 FA2 (
+
+project_1 FA2 (
 .a(a[2]),
 .b(b[2]),
-.c_in(c[2]),
+.c_in(c[1]),
 .s(s[2]),
-.c_out(c[3])
+.c_out(c[2])
 );
 project_1 FA3 (
 .a(a[3]),
 .b(b[3]),
-.c_in(c[3]),
+.c_in(c[2]),
 .s(s[3]),
 .c_out(c_out)
 );
