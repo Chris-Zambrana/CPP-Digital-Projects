7dce63829a297fcc0837eefed3e8b7fedd739590
Chris-Zambrana <crzambrana7@gmail.com>
2025-09-23 22:45:12 -0700

    Update shift_width_calc.v

--- Smartphone Projects/SoC Projects/QSPI/RTL/shift_width_calc.v
+++ Smartphone Projects/SoC Projects/QSPI/RTL/shift_width_calc.v
@@ -1,21 +1,23 @@
 `timescale 1ns / 1ps
 
+`include "qspi_definitions.vh"
 
 module shift_width_calc
 (
     input wire       clk,
     input wire       reset, 
-    input wire [2:0] mode,
+    input wire [1:0] mode,
     
-    output reg [2:0] shift_width
+    output reg [1:0] shift_width
 );
     
     always @(*) begin
         case (mode)
-            `MODE_SINGLE: shift_width = 3'd1;
-            `MODE_DUAL:   shift_width = 3'd2;
-            `MODE_QUAD:   shift_width = 3'd4;
-            default:      shift_width = 3'd1;
+            `MODE_ZERO:   shift_width = 0;
+            `MODE_SINGLE: shift_width = 1;
+            `MODE_DUAL:   shift_width = 2;
+            `MODE_QUAD:   shift_width = 4;
+            default:      shift_width = 0;
         endcase
     end
     
